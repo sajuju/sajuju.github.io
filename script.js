9be1f53ef6df791d670d388e6384ab09bf99dbb5
Bagas Agung Wiyono <zenkun.enterkill13@gmail.com>
2025-05-05 20:14:50 +0700

    Implement dropdown menu with hover animation and remove unused JavaScript

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.group').forEach(item => {
-    item.addEventListener('click', () => {
-        const submenu = item.querySelector('ul');
-        submenu.classList.toggle('hidden');
-    });
-});
